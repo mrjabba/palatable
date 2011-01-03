3b28ff1a970a3f1350a21097790d00410be625f2
mrjabba <mrjabba@gmail.com>
2011-01-03 16:27:50 -0600

    default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Palatable::Application.config.secret_token = '691f94634d94e211fb3a99c58cf47541bc1eb6181472a7344464c6bd078ea65e93a98deb68effbe5ec2bdd6eed16340756cfa322fb3e14b078d88d2f5b0d6d93'
