f128c69c06e673e115c1e6bc14c522651c5f26df
mrjabba <mrjabba@gmail.com>
2011-02-21 16:42:12 -0600

    fix for tag paging

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 $(function() {
-  $("#bookmarks th a, #bookmarks .pagination a").live("click", function() {
+  $("#bookmarks th a, #bookmarks .pagination a, #tags th a, #tags .pagination a").live("click", function() {
     $.getScript(this.href);
     return false;
   });
-  $("#bookmarks_search").submit(function() {
+  $("#bookmarks_search, #tags_search").submit(function() {
     $.get(this.action, $(this).serialize(), null, "script");
     return false;
   });  
